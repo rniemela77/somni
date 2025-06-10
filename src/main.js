ec5f12966fa01ad178c9f7e6b505a95c662ad4e0
rniemela77 <rvniemela@hotmail.com>
2025-06-10 16:41:47 -0700

    import bootstrap before global styling

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import { createPinia } from 'pinia';
 import App from "./App.vue";
 import router from "./router";
 import { useAuthStore } from "./stores/auth";
-import "./styles/global.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./styles/global.css";
 // Create Vue application
 const app = createApp(App);
 const pinia = createPinia();
