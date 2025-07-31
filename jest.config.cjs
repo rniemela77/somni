8c90cbf84904b452f574722c1d6ae2effb578571
rniemela77 <rvniemela@hotmail.com>
2025-07-30 20:02:44 -0700

    add user store test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.{js,ts}', '**/?(*.)+(spec|test).{js,ts}'],
   collectCoverageFrom: [
     'netlify/functions/**/*.js',
     'src/**/*.{js,vue,ts}',
@@ -10,13 +10,24 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
-  transform: {},
-  extensionsToTreatAsEsm: [],
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  
+  // Transform configuration
+  transform: {
+    '^.+\\.(js|ts)$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
+  },
+  
+  // Module file extensions
+  moduleFileExtensions: ['js', 'json', 'vue', 'ts'],
   
   // Handle CSS and other assets
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
-  }
+  },
+  
+  // Transform ignore patterns
+  transformIgnorePatterns: [
+    'node_modules/(?!(pinia|vue|@vue)/)'
+  ]
 }; 
